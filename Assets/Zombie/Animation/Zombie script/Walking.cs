1486fe27d29ed322c36cef1c3612992e90f174f7
DrNefario69 <filip.westerholm@edu.varberg.se>
2026-04-02 10:29:57 +0200

    Movement script tutorial 20 min

--- Assets/Zombie/Animation/Zombie script/Walking.cs
+++ Assets/Zombie/Animation/Zombie script/Walking.cs
@@ -1,16 +1,50 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Walking : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Zombie Things")]
+    public NavMeshAgent zombieAgent;
+    public Transform LookPoint;
+    public LayerMask PlayerLayer;
+
+    [Header("Zombie Guarding variable")]
+    public GameObject[] walkPoints;
+    int currentZombiePosition = 0;
+    public float zombieSpeed;
+    float walkingPointRadius = 2;
+
+    [Header("Zombie mood/states")]
+    public float visionRadius;
+    public float attackingRadius;
+    public bool playerInVisionRadius;
+    public bool playerInAttackingRadius;
+
+    private void Awake()
     {
-        
+        zombieAgent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        playerInVisionRadius = Physics.CheckSphere(transform.position, visionRadius, PlayerLayer);
+        playerInAttackingRadius = Physics.CheckSphere(transform.position, attackingRadius, PlayerLayer);
         
+        if (!playerInVisionRadius && !playerInAttackingRadius) Guard();
+    }
+
+    private void Guard()
+    {
+        if(Vector3.Distance(walkPoints[currentZombiePosition].transform.position, transform.position) < walkingPointRadius)
+        {
+            currentZombiePosition = Random.Range(0, walkPoints.Length);
+            if(currentZombiePosition >= walkPoints.Length)
+            {
+                currentZombiePosition = 0;
+
+            }
+        }
+       transform.position = Vector3.MoveTowards(transform.position, walkPoints[currentZombiePosition].transform.position, zombieSpeed * Time.deltaTime);
+        //Ändrar zombies hastighet och riktning mot den aktuella walkpointen, så att den rör sig mot den.
     }
 }
