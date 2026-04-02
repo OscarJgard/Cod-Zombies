07ca9515b58edb525f5c0b29ac2a3cfc20088805
OscarJgard <oscijebbe@gmail.com>
2026-04-02 10:27:05 +0200

    Camera

--- Assets/Character/Scripts/PlayerLook.cs
+++ Assets/Character/Scripts/PlayerLook.cs
@@ -2,15 +2,38 @@ using UnityEngine;
 
 public class PlayerLook : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Transform PlayerCamera;
+    public Vector2 Sensitivities;
+
+
+
+    private Vector2 XYRotation;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector2 MouseInput = new Vector2
+        {
+            x = Input.GetAxis("Mouse X"),
+            y = Input.GetAxis("Mouse Y")
+        };
+
+
+        XYRotation.x -= MouseInput.y * Sensitivities.y;
+        XYRotation.y += MouseInput.x * Sensitivities.x;
+
+
+        XYRotation.x = Mathf.Clamp(XYRotation.x, -90f, 90f);
+
+        transform.eulerAngles = new Vector3(0f, XYRotation.y, 0f);
+        PlayerCamera.localEulerAngles = new Vector3(XYRotation.x, 0f, 0f);
+
+
+
+
     }
 }
